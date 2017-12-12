4c80e8866a6f507f14fd94558e7814452d373280
Esoteric925 <haval925@gmail.com>
2017-12-12 16:12:27 +0100

    python CLASSPATH=$CLASSPATH:../../zookeeper-3.5.3-beta.jar

---
+++ venv/src/__main__.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+#hello = tf.constant('Hello, TensorFlow')
+#sess = tf.Session()
+#print (sess.run(hello))
+
+#a = tf.constant(42)
+#b = tf.constant(32)
+#print(sess.run(a+b))
